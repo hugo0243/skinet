d3b99a346a579eb61373067b1992354ae2d1640a
Hugo Osorio <hugo.o0243@gmail.com>
2020-10-15 20:56:24 -0500

    end section 8

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     // This is because the app will run in the browser
     BrowserModule,
     AppRoutingModule,
     // Modulo agregado por ngx-bootstrap para usar en animaciones
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
